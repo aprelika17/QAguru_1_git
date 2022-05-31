d51896cc70e7ae215e2d2cf876e9132363a3f9e3
Menkova Anastasiya <menkovaa17@gmail.com>
2022-06-01 01:56:50 +0300

    first commit

---
+++ src/test/java/quru/qa/FirstTest.java
@@ -0,0 +1,11 @@
+package quru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    void firsttest () {
+        Assertions.assertTrue(10>9);
+    }
+}
